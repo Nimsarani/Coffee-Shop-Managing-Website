db75b6063d9ced1790f31f18ea6410fa3c6919c8
malsha <nimsarani@gmail.com>
2025-04-01 21:02:49 +0530

    finalize

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -9,6 +10,10 @@ menuOpenButton.addEventListener("click",() => {
 //Close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click())
 
+//Close menu when the nav link is clicked
+navLinks.forEach(link =>{
+    link.addEventListener("click", ()=> menuOpenButton.click());
+});
 
 //Initialize Swiper
 const swiper = new Swiper('.slider-wrapper', {
